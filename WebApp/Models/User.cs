3d0a3d9168fea490ba6ee404799dde13ecb9e236
ofeka <ofeka@LAPTOP-F7UD0GCF>
2022-05-22 17:49:32 +0300

    Add project files.

---
+++ WebApp/Models/User.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebApp
+{
+    public class User
+    {
+        [Key]
+        public string name { get; set; }
+        public string nick { get; set; }
+
+        [DataType(DataType.Password)]
+        public string password { get; set; }
+
+        public string image { get; set; }
+
+        public List<Contact> chats { get; set; }
+
+    }
+}
